0e2ad6bec48caba62d6ae5307abd1ed245882dde
Ubuntu <azureuser@SaloniUbuntu.vzx4qmicrjoelfyjxkrxrkuyzc.cx.internal.cloudapp.net>
2025-01-29 13:05:21 +0000

    Initial commit

---
+++ src/main/java/com/example/usermanagement/models/User.java
@@ -0,0 +1,14 @@
+package com.example.usermanagement.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Getters and Setters
+}
